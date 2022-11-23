0a8ad766d3979eb3c19511141ec3774c9b6e7da1
marguerite <i@marguerite.su>
2022-11-23 15:07:53 +0800

    can build with googlefonts' fontmake now

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+for i in `WenQuanYiZenHei-01.ufo3 WenQuanYiZenHeiMono-02.ufo3 WenQuanYiZenHeiSharp-03.ufo3`; do
+  /usr/bin/fontmake -u $i --validate-ufo --overlaps-backend pathops --verbose DEBUG
+done
