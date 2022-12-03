fdce608049fbc721ed3d9363a35b6de23c09d160
marguerite <i@marguerite.su>
2022-12-03 21:38:14 +0800

    fix FAIL found by fontbakery

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-for i in `WenQuanYiZenHei-01.ufo3 WenQuanYiZenHeiMono-02.ufo3 WenQuanYiZenHeiSharp-03.ufo3`; do
-  /usr/bin/fontmake -u $i --validate-ufo --overlaps-backend pathops --verbose DEBUG
+for i in "WenQuanYiZenHei-01.ufo3" "WenQuanYiZenHeiMono-02.ufo3" "WenQuanYiZenHeiSharp-03.ufo3"; do
+  /usr/bin/fontmake -u $i --validate-ufo --overlaps-backend pathops -a --verbose DEBUG -o otf-cff2 ttf
 done
